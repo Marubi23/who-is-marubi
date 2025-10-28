6d23f0128d2216fa85934eaaee69a56196e6d8a6
Marubi 254 <felixmarubi2005@gmail.com>
2025-10-28 06:42:16 +0300

    portifolio changes

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -18,10 +18,11 @@ interface Project {
 })
 export class ProjectsComponent {
   projects: Project[] = [
+        { name: 'MindsBridge', description: 'MindsBridge is a platform built to aid learning support and psychiatrist help regardless of the situation.A key feature to boost education systems in Kenya', tools: ['Angular','Bootstrap','Render(rendering)'], year: 2025, link: 'https://mind-bridge.co.ke/' },
     { name: 'VarsityHub', description: 'An E-commerce site that provides a platform for young sellers to showcase their products on a local area.', tools: ['Angular','Node.js','MongoDB'], year: 2025, link: 'https://kyuvarsityhub.netlify.app' },
     { name: 'Minds That Matter', description: 'Accessible learning platform powered by AI-driven engagement.', tools: ['React','Express','MongoDB'], year: 2025, link: 'https://mindsthatmatter.netlify.app' },
-    { name: 'ByteLab', description: 'ByteLabs is a hands-on cybersecurity learning platform that provides practical labs, guided mentorship, and global community support for aspiring ethical hackers and IT security enthusiasts', tools: ['Angular','Bootstrap','Nelify(rendering)'], year: 2025, link: 'https://marubilabs.netlify.app/' },
-    { name: 'MindsBridge', description: 'MindsBridge is a platform built to aid learning support and psychiatrist help regardless of the situation.A key feature to boost education systems in Kenya', tools: ['Angular','Bootstrap','Render(rendering)'], year: 2025, link: 'https://mindbridge-avg7.onrender.com/' }
+    { name: 'ByteLab', description: 'ByteLabs is a hands-on cybersecurity learning platform that provides practical labs, guided mentorship, and global community support for aspiring ethical hackers and IT security enthusiasts', tools: ['Angular','Bootstrap','Nelify(rendering)'], year: 2025, link: 'https://marubilabs.netlify.app/' }
+
   ];
 
   modalOpen = false;
